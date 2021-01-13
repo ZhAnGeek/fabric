d219e2438a1d5c3f3164c4c515dad32e14ad9123
zhangsk01 <zhangsk01@fenbi.com>
2021-01-13 10:02:10 +0800

    Debug2

--- orderer/consensus/pbft/node/request.go
+++ orderer/consensus/pbft/node/request.go
@@ -1,6 +1,7 @@
 package node
 
 import (
+	"github.com/hyperledger/fabric/orderer/consensus/pbft/message"
 	"log"
 )
 
@@ -8,8 +9,10 @@ func (n *Node) requestRecvThread() {
 	log.Printf("[Node] start recv the request thread")
 	for {
 		msg := <- n.requestRecv
-		log.Printf("[Node] msg := %v", msg)
 		// check is primary
+		log.Printf("[Node] msg := %v", n.IsPrimary())
+		log.Printf("[Node] msg := %v", n.GetPrimary())
+		log.Printf("[Node] msg := %v", message.Identify(n.view))
 		if !n.IsPrimary() {
 			if n.lastReply.Equal(msg) {
 				// TODO just reply
